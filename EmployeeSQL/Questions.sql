f5b424ded7f1090a806fb103193530d199fa1254
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-05-17 22:54:04 -0400

    adding new files

--- EmployeeSQL/Questions.sql
+++ EmployeeSQL/Questions.sql
@@ -11,3 +11,12 @@ SELECT first_name, last_name, hire_date
 FROM "Employees"
 WHERE hire_date >= '01/01/1986'
 AND hire_date <= '01/01/1987'
+
+-- 3.
+
+SELECT de.dept_no, d.dept_name, de.emp_no, e.first_name, e.last_name
+FROM "Dept_Manager" as de
+INNER Join "Departments" as d
+ON de.dept_no = d.dept_no
+INNER Join "Employees" as e
+ON e.emp_no = de.emp_no;
